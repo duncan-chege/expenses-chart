48cdb8ebe63b932f11c5495d35ad9311a8381e83
viisualworks <dshege4@gmail.com>
2024-09-11 13:30:05 +0300

    customize tailwind theme properties for my project

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "DM Sans, sans-serif",
+      },
+      fontSize: {
+        base: '18px',
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+      colors: {
+        "soft-red": "hsl(10, 79%, 65%)",
+        "cyan": "hsl(186, 34%, 60%)",
+        "dark-brown": "hsl(25, 47%, 15%)",
+        "medium-brown": "hsl(28, 10%, 53%)",
+        "cream": "hsl(27, 66%, 92%)",
+        "pale-orange": "hsl(33, 100%, 98%)",
+      }
+    },
   },
   plugins: [],
 }
