87182b7656cd37fa1b1be4991a3892012cf502f7
viisualworks <dshege4@gmail.com>
2024-09-11 22:57:27 +0300

    add a bar graph component and lift the state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import { BarGraph } from "./BarGraph";
+import { useState } from "react";
+import BarGraph from "./BarGraph";
 
 const App = () => {
+  const [expenseSum, setExpenseSum] = useState("");
+
+  const handleExpenseSum = (data) => {
+    setExpenseSum(data)
+  }
+
 return (
 <div className="h-screen bg-cream font-sans grid place-items-center grid-cols-1">
-  <div className="w-1/3">
+  <div className="xl:w-1/3 md:w-2/3 w-11/12">
     <div className="mb-8 py-6 px-8 bg-soft-red text-white rounded-xl flex justify-between">
       <div>
         <p className="font-light pb-1">My balance</p>
@@ -11,9 +18,19 @@ return (
       </div>
       <img src="../public/assets/images/logo.svg" alt="" />
     </div>
+
     <div className="py-6 px-8 rounded-xl bg-pale-orange">
       <h3 className="text-2xl font-bold text-dark-brown"> Spending- Last 7 days</h3>
-      <BarGraph />
+      <BarGraph sendTotalSum = {handleExpenseSum} />
+      <hr className="border-1" />
+      <p className="mt-6 text-stone-400">Total This Month</p>
+      <div className="flex justify-between">
+      <h2 className="text-3xl md:text-5xl mt-1 font-bold text-dark-brown">${expenseSum}</h2>
+      <div className="text-right">
+        <p className="text-dark-brown font-bold">+2.4%</p>
+        <p className="text-stone-400 -mt-1">from last month</p>
+      </div>
+      </div>
     </div>
   </div>
 </div>
--- src/BarGraph.jsx
+++ src/BarGraph.jsx
@@ -1,10 +1,78 @@
 import { Chart as ChartJS } from "chart.js/auto";
+import { Bar } from "react-chartjs-2";
+import expenseData from "./data.json";
+import { useEffect } from "react";
 
-const BarGraph = () => {
+const BarGraph = ({sendTotalSum}) => {
+
+    const currentDay ="wed";
+
+    const totalSum = expenseData.reduce((accumulator, current) => accumulator + current.amount, 0);
+
+    useEffect(() => {
+        sendTotalSum(totalSum);
+    }, [sendTotalSum]);
+
     return (
-        <>
-        
-        </>
+        <Bar 
+            data={{
+                labels: expenseData.map((data) => data.day),
+                datasets: [
+                    {
+                        data: expenseData.map((data) => data.amount),
+                        backgroundColor: expenseData.map((data) => data.day === currentDay ? "hsl(186, 34%, 60%)" : "hsl(10, 79%, 65%)"),
+                        hoverBackgroundColor: expenseData.map((data) => data.day === currentDay ? "#76b5bcbf" : "hsl(10, 79%, 75%)"),
+                        borderRadius: 5,
+                    }
+                ],
+            }}
+            options= {{
+                responsive: true,
+                maintainAspectRatio: true,
+                layout: {
+                    padding: {
+                        top: 20,
+                        bottom: 20
+                    }
+                },
+                plugins: {
+                    legend: {
+                        display: false,
+                    },
+                    tooltip: {
+                        yAlign: "bottom",
+                        caretSize: 0,
+                        caretPadding: 6,
+                        callbacks: {
+                            title: function() {
+                                return "";
+                            },
+                            label: function(context) {
+                                return `$${context.raw}`
+                            }
+                        },
+                        displayColors: false,
+                    },
+                },
+                scales: {
+                    x: {
+                        grid: {
+                            display: false,
+                        }
+                    },
+                    y: {
+                        grid: {
+                            display: false,
+                        },
+                        display: false
+                    }
+                }
+            }}    
+            
+
+        />
+
+
     )
 }
 
